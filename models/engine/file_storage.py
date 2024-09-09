52c9813a3da8ee764d0b85e7698e06b5a52a8231
Victoria Fakunle <v.fakunle@alustudent.com>
2024-09-09 22:44:38 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+import json
+import os
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj.to_dict()
+
+    def save(self):
+        with open(self.__file_path, "w") as f:
+            json.dump(self.__objects, f)
+
+    def reload(self):
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                self.__objects = json.load(f)
