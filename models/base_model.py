ce4663c16d16292dc7d9f82b8966913fbfe4dc39
Victoria Fakunle <v.fakunle@alustudent.com>
2024-09-09 23:53:14 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,14 @@
-import uuid
-from datetime import datetime
+from models import storage
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ["created_at", "updated_at"]:
-                        value = datetime.fromisoformat(value)
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    # ...
 
     def save(self):
-        self.updated_at = datetime.now()
+        storage.new(self)
+        storage.save()
 
-    def to_dict(self):
-        dict_repr = self.__dict__.copy()
-        dict_repr["__class__"] = self.__class__.__name__
-        dict_repr["created_at"] = self.created_at.isoformat()
-        dict_repr["updated_at"] = self.updated_at.isoformat()
-        return dict_repr
+    def __init__(self, *args, **kwargs):
+        if not kwargs:
+            # Create a new instance
+            storage.new(self)
+        super().__init__(*args, **kwargs)
