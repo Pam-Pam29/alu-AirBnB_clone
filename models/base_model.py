98bf858db4dc7d127566e5e343ef8fb9d7760648
Victoria Fakunle <v.fakunle@alustudent.com>
2024-09-10 00:27:57 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 import uuid
 from datetime import datetime
-import models
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -15,20 +14,3 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-
-    def save(self):
-        """Updates the public instance attribute updated_at and saves the instance to the storage."""
-        self.updated_at = datetime.now()
-        models.storage.save()
-
-    def to_dict(self):
-        """Returns a dictionary containing all keys/values of the instance."""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
-
-    def __str__(self):
-        """Returns the string representation of the instance."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
